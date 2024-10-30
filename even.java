c40083bbf15af9953d651326762fb3f97062fb9d
Ganesh Mahind <ganeshmahind30@gmail.com>
2024-10-30 11:01:08 +0530

    doing

---
+++ even.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public  class even{
+
+    public static void main(String[] args) {
+        int n;
+     Scanner m= new Scanner(System.in);
+        n=m.nextInt();
+        for(int i=0;i<=n;i=i+2){
+            System.out.println(i);
+        }
+    }
+}
